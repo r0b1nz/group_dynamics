a9e7ef2f835ebb5455c2eee4f1520e7bf64cc524
nemesis_1418 <akashattri.chd@gmail.com>
2017-09-29 23:57:12 +0530

    refactor

--- group_project/mobile/urls.py
+++ group_project/mobile/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url, include
 from rest_framework import routers
-from views import Register, UserLoginAPIView
+from mobile.views import Register, UserLoginAPIView
 
 router = routers.DefaultRouter()
 router.register(r'register', Register, base_name='mobile_signup')
--- group_project/mobile/views.py
+++ group_project/mobile/views.py
@@ -1,7 +1,8 @@
-import rest_framework
 from django.contrib.auth import authenticate
-from rest_framework import serializers
 from django.contrib.auth.models import User
+
+import rest_framework
+from rest_framework import serializers
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
@@ -92,6 +93,3 @@ class UserLoginAPIView(APIView):
             new_data = serializer.data
             return Response(new_data,status=HTTP_200_OK)
         return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
-
-
-
